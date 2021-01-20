17e3a16368359584e805435edbaa50abf1789ab7
Charles <Charles@0x7f.cc>
2021-01-21 03:22:18 +0800

    Fix random bug

--- modules/RandomCat.py
+++ modules/RandomCat.py
@@ -10,13 +10,14 @@ enabled = True
 def load():
     print("RandomCat Plugin Loaded!")
 
+
 def run(update: Update, context: CallbackContext) -> None:
     if len(context.args) != 0:
         update.message.reply_text(
             "*Get a random cat from thiscatdoesnotexist.com .*\nUsage: `/cat`.", parse_mode='Markdown')
     else:
         update.message.reply_photo(
-            "https://thiscatdoesnotexist.com")
+            "https://thiscatdoesnotexist.com/?rnd={}".format(str(random.randint(0, 31415926))))
 
 
 handlers = [CommandHandler("cat", run)]
--- modules/RandomPerson.py
+++ modules/RandomPerson.py
@@ -10,13 +10,14 @@ enabled = True
 def load():
     print("RandomPerson Plugin Loaded!")
 
+
 def run(update: Update, context: CallbackContext) -> None:
     if len(context.args) != 0:
         update.message.reply_text(
             "*Get a random person from thispersondoesnotexist.com .*\nUsage: `/person`.", parse_mode='Markdown')
     else:
         update.message.reply_photo(
-            "https://thispersondoesnotexist.com/image")
+            "https://thispersondoesnotexist.com/image?rnd={}".format(str(random.randint(0, 31415926))))
 
 
 handlers = [CommandHandler("person", run)]
