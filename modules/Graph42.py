8726100ca477139b7db41c9a5e4efd25c57627c8
Charles <Charles@0x7f.cc>
2021-05-24 15:19:32 +0800

    Add as path lookup

--- modules/Graph42.py
+++ modules/Graph42.py
@@ -15,28 +15,42 @@ def load():
     print("Graph42 Plugin Loaded!")
 
 
-def getImage(asn):
-    if asn.isnumeric() or (asn[:2].upper() == "AS" and asn[2:].isnumeric()):
-        try:
-            u = "http://info-api.nia.dn42/as_graph/{}".format(asn)
-            r = requests.get(u, stream=True)
-            if r.status_code != 200:
+def getImage(asn, asn2=None):
+    if asn2:
+        if (asn.isnumeric() or (asn[:2].upper() == "AS" and asn[2:].isnumeric())) and (asn2.isnumeric() or (asn2[:2].upper() == "AS" and asn2[2:].isnumeric())):
+            try:
+                u = "http://info-api.nia.dn42/path_lookup/{}/{}".format(
+                    asn, asn2)
+                r = requests.get(u, stream=True)
+                if r.status_code != 200:
+                    return None
+                with open(os.getenv("MODULE_GRAPH42_TMPIMG") + ".svg", "wb") as f:
+                    r.raw.decode_content = True
+                    shutil.copyfileobj(r.raw, f)
+                return os.getenv("MODULE_GRAPH42_TMPIMG") + ".svg"
+            except Exception as e:
                 return None
-            with open(os.getenv("MODULE_GRAPH42_TMPIMG") + ".svg", "wb") as f:
-                r.raw.decode_content = True
-                shutil.copyfileobj(r.raw, f)
-            return os.getenv("MODULE_GRAPH42_TMPIMG") + ".svg"
-        except Exception as e:
+        else:
             return None
     else:
-        return None
+        if asn.isnumeric() or (asn[:2].upper() == "AS" and asn[2:].isnumeric()):
+            try:
+                u = "http://info-api.nia.dn42/as_graph/{}".format(asn)
+                r = requests.get(u, stream=True)
+                if r.status_code != 200:
+                    return None
+                with open(os.getenv("MODULE_GRAPH42_TMPIMG") + ".svg", "wb") as f:
+                    r.raw.decode_content = True
+                    shutil.copyfileobj(r.raw, f)
+                return os.getenv("MODULE_GRAPH42_TMPIMG") + ".svg"
+            except Exception as e:
+                return None
+        else:
+            return None
 
 
 def run(update: Update, context: CallbackContext) -> None:
-    if len(context.args) != 1:
-        update.message.reply_text(
-            "*Get dn42 path graph.*\nUsage: `/graph42 {ASN}`.", parse_mode="Markdown")
-    else:
+    if len(context.args) == 1:
         context.bot.sendChatAction(
             chat_id=update.message.chat_id, action=ChatAction.UPLOAD_PHOTO)
         im = getImage(context.args[0])
@@ -47,6 +61,20 @@ def run(update: Update, context: CallbackContext) -> None:
                 update.message.reply_photo(f)
         else:
             update.message.reply_text("`Not found.`", parse_mode='Markdown')
+    elif len(context.args) == 2:
+        context.bot.sendChatAction(
+            chat_id=update.message.chat_id, action=ChatAction.UPLOAD_PHOTO)
+        im = getImage(context.args[0], context.args[1])
+        if im:
+            svg2png(open(im, 'rb').read(), write_to=open(
+                os.getenv("MODULE_GRAPH42_TMPIMG") + ".png", 'wb'))
+            with open(os.getenv("MODULE_GRAPH42_TMPIMG") + ".png", "rb") as f:
+                update.message.reply_photo(f)
+        else:
+            update.message.reply_text("`Not found.`", parse_mode='Markdown')
+    else:
+        update.message.reply_text(
+            "*Get dn42 path graph.*\nUsage: `/graph42 {ASN} [to ASN]`.", parse_mode="Markdown")
 
 
 handlers = [CommandHandler("graph42", run, run_async=True)]
