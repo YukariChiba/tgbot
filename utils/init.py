72203769d3114d3f18a85675dc58f13f6d791054
Charles <Charles@0x7f.cc>
2021-07-22 02:20:41 +0800

    Fix bug in creating tmp dir

--- utils/init.py
+++ utils/init.py
@@ -4,6 +4,6 @@ def chk_dir(dir):
     Path(dir).mkdir(parents=True, exist_ok=True)
 
 def init():
-    chk_dir("cache")
-    chk_dir("cache/tmp")
-    chk_dir("cache/user_photo")
+    chk_dir(os.getenv("CACHE_DIR"))
+    chk_dir(os.getenv("CACHE_DIR") + "/tmp")
+    chk_dir(os.getenv("CACHE_DIR") + "/user_photo")
