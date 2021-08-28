ee5ec19cc1a8316cdbdc9ddf9812e0f0bcfa65a4
David Wang <david.wang@kirinou.top>
2021-08-29 01:01:51 +0800

    Fix Fortune Telling Module (#3)
    * delete unused var
    
    * Fix bugs

--- modules/FortuneTelling.py
+++ modules/FortuneTelling.py
@@ -1,5 +1,5 @@
 from telegram.ext import CallbackContext, CommandHandler, CallbackQueryHandler
-from telegram import Update, ChatAction, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import Update, ChatAction, InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
 from telegram.utils.helpers import escape_markdown
 import random
 import os
@@ -7,19 +7,18 @@ import json
 
 enabled = True
 
-randint = 0
-
 def load():
     print("FortuneTelling Plugin Loaded!")
 
 def run(update: Update, context: CallbackContext) -> None:
     page = random.randint(1, 100)
+    page_1 = InputMediaPhoto("https://raw.githubusercontent.com/fumiama/senso-ji-omikuji/main/{}_0.jpg".format(str(page)))
+    page_2 = InputMediaPhoto("https://raw.githubusercontent.com/fumiama/senso-ji-omikuji/main/{}_1.jpg".format(str(page)))
     if len(context.args) != 0:
         update.message.reply_text(
             "*Get a senso ji omikuji*\nUsage: `/senso`.", parse_mode='Markdown')
     else:
-        update.message.reply_media_group(["https://raw.githubusercontent.com/fumiama/senso-ji-omikuji/main/{}_0.jpg".format(str(page)),
-        "https://raw.githubusercontent.com/fumiama/senso-ji-omikuji/main/{}_1.jpg".format(str(page))])
+        update.message.reply_media_group([page_1, page_2])
 
 
 handlers = [CommandHandler("senso", run, run_async=True)]
