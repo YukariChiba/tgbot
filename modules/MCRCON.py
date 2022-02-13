059dbbe81fffc4684be2a1d90d828aca18f156be
Yukari Chiba <i@0x7f.cc>
2022-02-13 13:46:16 +0800

    Add MCRCON related function

--- modules/MCRCON.py
+++ modules/MCRCON.py
@@ -18,8 +18,15 @@ def args_check_banlist(args):
         return False
 
 
-def args_check_ban(args):
+def args_check_pardon(args):
     if len(args) == 1:
+        return True
+    else:
+        return False
+
+
+def args_check_ban(args):
+    if len(args) == 1 or len(args) == 2:
         return True
     else:
         return False
@@ -30,7 +37,11 @@ def kick(update: Update, context: CallbackContext) -> None:
         if args_check_ban(context.args):
             try:
                 with Client(os.getenv("MODULE_MCRCON_SERVER"), 25575, passwd=os.getenv("MODULE_MCRCON_PASS")) as client:
-                    resp = client.kick(context.args[0])
+                    if len(context.args) == 2:
+                        resp = client.kick(
+                            context.args[0], context.args[1])
+                    else:
+                        resp = client.kick(context.args[0])
                     update.message.reply_text(
                         "`" + resp + "`", parse_mode='Markdown')
             except Exception as e:
@@ -38,7 +49,27 @@ def kick(update: Update, context: CallbackContext) -> None:
                     "`Server Error: {}`".format(type(e).__name__), parse_mode='Markdown')
         else:
             update.message.reply_text(
-                "*Kick specific player from MC server.*\nUsage: `/mckick <Player>`.", parse_mode='Markdown')
+                "*Kick specific player from MC server.*\nUsage: `/mckick <Player> [Reason]`.", parse_mode='Markdown')
+
+
+def banip(update: Update, context: CallbackContext) -> None:
+    if str(update.message.from_user.id) == str(os.getenv("MODULE_MCRCON_ADMIN")):
+        if args_check_ban(context.args):
+            try:
+                with Client(os.getenv("MODULE_MCRCON_SERVER"), 25575, passwd=os.getenv("MODULE_MCRCON_PASS")) as client:
+                    if len(context.args) == 2:
+                        resp = client.banip(
+                            context.args[0], context.args[1])
+                    else:
+                        resp = client.banip(context.args[0])
+                    update.message.reply_text(
+                        "`" + resp + "`", parse_mode='Markdown')
+            except Exception as e:
+                update.message.reply_text(
+                    "`Server Error: {}`".format(type(e).__name__), parse_mode='Markdown')
+        else:
+            update.message.reply_text(
+                "*Ban specific IP from MC server.*\nUsage: `/mcbanip <IP> [Reason]`.", parse_mode='Markdown')
 
 
 def ban(update: Update, context: CallbackContext) -> None:
@@ -46,7 +77,11 @@ def ban(update: Update, context: CallbackContext) -> None:
         if args_check_ban(context.args):
             try:
                 with Client(os.getenv("MODULE_MCRCON_SERVER"), 25575, passwd=os.getenv("MODULE_MCRCON_PASS")) as client:
-                    resp = client.ban(context.args[0])
+                    if len(context.args) == 2:
+                        resp = client.ban(
+                            context.args[0], context.args[1])
+                    else:
+                        resp = client.ban(context.args[0])
                     update.message.reply_text(
                         "`" + resp + "`", parse_mode='Markdown')
             except Exception as e:
@@ -54,7 +89,23 @@ def ban(update: Update, context: CallbackContext) -> None:
                     "`Server Error: {}`".format(type(e).__name__), parse_mode='Markdown')
         else:
             update.message.reply_text(
-                "*Ban specific player from MC server.*\nUsage: `/mcban <Player>`.", parse_mode='Markdown')
+                "*Ban specific player from MC server.*\nUsage: `/mcban <Player> [Reason]`.", parse_mode='Markdown')
+
+
+def pardon(update: Update, context: CallbackContext) -> None:
+    if str(update.message.from_user.id) == str(os.getenv("MODULE_MCRCON_ADMIN")):
+        if args_check_pardon(context.args):
+            try:
+                with Client(os.getenv("MODULE_MCRCON_SERVER"), 25575, passwd=os.getenv("MODULE_MCRCON_PASS")) as client:
+                    resp = client.pardon(context.args[0])
+                    update.message.reply_text(
+                        "`" + resp + "`", parse_mode='Markdown')
+            except Exception as e:
+                update.message.reply_text(
+                    "`Server Error: {}`".format(type(e).__name__), parse_mode='Markdown')
+        else:
+            update.message.reply_text(
+                "*Removes specific player from banlist of MC server.*\nUsage: `/pardon <Player>`.", parse_mode='Markdown')
 
 
 def banlist(update: Update, context: CallbackContext) -> None:
@@ -74,5 +125,7 @@ def banlist(update: Update, context: CallbackContext) -> None:
 
 
 handlers = [CommandHandler("mcban", ban, run_async=True),
+            CommandHandler("mcbanip", ban, run_async=True),
+            CommandHandler("mcpardon", pardon, run_async=True),
             CommandHandler("mckick", kick, run_async=True),
             CommandHandler("mcbanlist", banlist, run_async=True)]
