d0da78146b17b327068946e8b0bb55bc23d04b83
Yukari Chiba <i@0x7f.cc>
2024-12-28 15:47:02 +0800

    add keifuk

--- modules/FortuneTelling.py
+++ modules/FortuneTelling.py
@@ -10,7 +10,7 @@ enabled = True
 def load():
     print("FortuneTelling Plugin Loaded!")
 
-def run(update: Update, context: CallbackContext) -> None:
+def run_senso(update: Update, context: CallbackContext) -> None:
     page = random.randint(1, 100)
     page_1 = InputMediaPhoto("https://raw.githubusercontent.com/fumiama/senso-ji-omikuji/main/{}_0.jpg".format(str(page)))
     page_2 = InputMediaPhoto("https://raw.githubusercontent.com/fumiama/senso-ji-omikuji/main/{}_1.jpg".format(str(page)))
@@ -20,5 +20,16 @@ def run(update: Update, context: CallbackContext) -> None:
     else:
         update.message.reply_media_group([page_1, page_2])
 
+def run_keifuk(update: Update, context: CallbackContext) -> None:
+    page = random.randint(1, 100)
+    page_1 = "https://taonet.siksikyuen.org.hk/images/StickEnquiry/stickPaper/{}.png".format(str(page))
+    if len(context.args) != 0:
+        update.message.reply_text(
+            "*Get a siksikyuen keifuk*\nUsage: `/siksikyuen`.", parse_mode='Markdown')
+    else:
+        update.message.reply_photo(page_1, caption="https://taonet.siksikyuen.org.hk/StickEnquiry/{}/zh-TW".format(str(page)), parse_mode="Markdown")
 
-handlers = [CommandHandler("senso", run, run_async=True)]
+handlers = [
+    CommandHandler("senso", run_senso, run_async=True),
+    CommandHandler("keifuk", run_keifuk, run_async=True)
+]
